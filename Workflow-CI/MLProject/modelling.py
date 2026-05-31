1a265c0212a0d7d059c29970b2aee4c95ba6c5af
ForReal-Yuki <storagegwejh@gmail.com>
2026-05-31 14:51:51 +0700

    Menyelesaikan Kriteria 3 Level Advance (CI Workflow & Docker)

---
+++ Workflow-CI/MLProject/modelling.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import mlflow
+import mlflow.sklearn
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+df = pd.read_csv("Data Prepocessing.csv")
+X = df.drop(columns="Rent")
+y = df["Rent"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+mlflow.sklearn.autolog()
+
+with mlflow.start_run(run_name="CI_Automated"):
+    model = LinearRegression()
+    model.fit(X_train, y_train)
+    print("Model Linier selesai dilatih")
+
+    if os.path.exist("model_dir"):
+        shutil.rmtree("model_dir")
+    mlflow.sklearn.log_model(model, "model_dir")
+    print("Task Selesai ada di model_dir")
