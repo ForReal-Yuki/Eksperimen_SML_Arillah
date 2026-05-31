eee6cad9157782cdc41695cdef3ad920d528aa1d
ForReal-Yuki <storagegwejh@gmail.com>
2026-05-31 15:22:09 +0700

    Fix MLflow ghost path and use save_model

--- Workflow-CI/MLProject/modelling.py
+++ Workflow-CI/MLProject/modelling.py
@@ -4,22 +4,26 @@ import mlflow.sklearn
 import os
 import shutil
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
 
-df = pd.read_csv("Data Prepocessing.csv")
-X = df.drop(columns="Rent")
-y = df["Rent"]
+mlflow.set_tracking_uri("file://" + os.path.abspath("mlruns_temp"))
 
+#Load Data
+df = pd.read_csv('Data Prepocessing.csv')
+X = df.drop(columns=['Rent'])
+y = df['Rent']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 mlflow.sklearn.autolog()
 
-with mlflow.start_run(run_name="CI_Automated"):
-    model = LinearRegression()
+with mlflow.start_run(run_name="CI_Automated_Run"):
+    model = RandomForestRegressor(random_state=42)
     model.fit(X_train, y_train)
-    print("Model Linier selesai dilatih")
 
+    #Siapkan folder statis
     if os.path.exists("model_dir"):
         shutil.rmtree("model_dir")
-    mlflow.sklearn.log_model(model, "model_dir")
-    print("Task Selesai ada di model_dir")
+
+    #PENTING: Harus save_model agar Docker bisa baca, BUKAN log_model
+    mlflow.sklearn.save_model(model, "model_dir")
+    print("Training selesai dan model fisik disimpan di model_dir")
